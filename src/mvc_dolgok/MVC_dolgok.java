f8941f2ad2b77734e647d15e2cfed3d0d7403a06
WeimperGergo <weimper.geri@gmail.com>
2024-10-07 13:11:19 +0200

    mvc_dolg init

---
+++ src/mvc_dolgok/MVC_dolgok.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mvc_dolgok;
+
+/**
+ *
+ * @author weimper.g.zsombor
+ */
+public class MVC_dolgok {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
